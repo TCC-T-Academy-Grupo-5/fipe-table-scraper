cd4451769f46c7cba64726e902dded4e5ec62103
Paulo Henrique <ph.1928@hotmail.com>
2024-09-21 15:14:33 -0300

    feat: Brand repository

--- src/main/java/com/tcc/demoveiculos/repositories/BrandRepository.java
+++ src/main/java/com/tcc/demoveiculos/repositories/BrandRepository.java
@@ -1,7 +1,9 @@
 package com.tcc.demoveiculos.repositories;
 
 import com.tcc.demoveiculos.models.Brand;
+import com.tcc.demoveiculos.models.VehicleType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BrandRepository extends JpaRepository<Brand, String> {
+    long countAllByVehicleType(VehicleType vehicleType);
 }
