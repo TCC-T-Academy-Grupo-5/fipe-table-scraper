ad21b3742cbc87259504fe3a87e77b3a888b094f
Paulo Henrique <ph.1928@hotmail.com>
2024-09-21 20:31:03 -0300

    feat: load models

--- src/main/java/com/tcc/demoveiculos/system/ApiTest.java
+++ src/main/java/com/tcc/demoveiculos/system/ApiTest.java
@@ -5,14 +5,19 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tcc.demoveiculos.models.Brand;
 import com.tcc.demoveiculos.models.FipeMonthReference;
+import com.tcc.demoveiculos.models.Model;
+import com.tcc.demoveiculos.models.VehicleType;
 import com.tcc.demoveiculos.repositories.BrandRepository;
 import com.tcc.demoveiculos.repositories.FipeMonthReferenceRepository;
+import com.tcc.demoveiculos.repositories.ModelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,22 +34,28 @@ public class ApiTest implements CommandLineRunner {
     private BrandRepository brandRepository;
 
     @Autowired
+    private ModelRepository modelRepository;
+
+    @Autowired
     private ObjectMapper objectMapper;
 
     private Logger logger = Logger.getLogger(ApiTest.class.getName());
 
     private WebClient webClient = WebClient.builder().baseUrl("https://fipe.parallelum.com.br/api/v2").build();
 
+    @Value("${fipe-api.subscription.token}")
+    private String subscriptionToken;
+
     @Override
+    @Transactional
     public void run(String... args) throws Exception {
         if (this.fipeMonthReferenceRepository.count() == 0) {
             this.populateReferences();
         }
 
-        if (this.brandRepository.count() == 0) {
-            this.populateBrands();
-        }
+        this.populateBrands();
 
+        this.populateModels();
     }
 
     private void populateReferences() {
@@ -64,57 +75,110 @@ public class ApiTest implements CommandLineRunner {
 
         webClient.get()
                 .uri("/references")
+                .header("X-Subscription-Token", this.subscriptionToken)
                 .retrieve()
                 .bodyToMono(String.class)
-                .subscribe(result -> {
+                .flatMap(result -> {
                     try {
-                        List<FipeMonthReference> references = this.objectMapper.readValue(result, new TypeReference<>() {});
+                        List<FipeMonthReference> references = this.objectMapper.readValue(result, new TypeReference<>() {
+                        });
 
-                        references.stream().peek(ref -> {
+                        references.forEach(ref -> {
+                            this.logger.log(Level.INFO, "Preparando: " + ref.getMonth());
                             String month = ref.getMonth();
                             String[] monthAndYear = month.split("/");
                             String monthStr = monthAndYear[0];
                             String monthNumber = months.get(monthStr);
                             ref.setMonth(monthNumber + "/" + monthAndYear[1]);
-                        }).forEach(ref -> {
-                            this.logger.log(Level.INFO, "Salvando: " + ref.getMonth());
-                            this.fipeMonthReferenceRepository.save(ref);
                         });
+
+                        return Mono.just(this.fipeMonthReferenceRepository.saveAll(references));
                     } catch (JsonProcessingException e) {
                         this.logger.log(Level.SEVERE, "Erro ao converter JSON: " + e.getMessage());
+                        return Mono.error(e);
                     }
-                });
+                })
+                .subscribe(
+                        success -> this.logger.log(Level.INFO, "Todas as referências foram salvas no banco de dados"),
+                        error -> this.logger.log(Level.SEVERE, "Erro ao salvar referências: " + error.getMessage())
+                );
     }
 
     private void populateBrands() {
-        this.populateCarBrands();
+        if (this.brandRepository.count() == 0) {
+            this.populateBrandsByType("/cars/brands", VehicleType.CAR);
+            this.populateBrandsByType("/motorcycles/brands", VehicleType.MOTORCYCLE);
+            this.populateBrandsByType("/trucks/brands", VehicleType.TRUCK);
+        }
     }
 
-    private void populateCarBrands() {
+    private void populateBrandsByType(String uri, VehicleType vehicleType) {
         webClient.get()
-                .uri("/cars/brands")
+                .uri(uri)
+                .header("X-Subscription-Token", this.subscriptionToken)
                 .retrieve()
                 .bodyToMono(String.class)
-                .subscribe(result -> {
+                .flatMap(result -> {
                     try {
-                        List<Brand> carBrands = this.objectMapper.readValue(result, new TypeReference<>() {});
-
-                        carBrands.forEach(brand -> {
-                            this.logger.log(Level.INFO, "Salvando: " + brand.getName());
-                            this.brandRepository.save(brand);
+                        List<Brand> brands = this.objectMapper.readValue(result, new TypeReference<>() {
                         });
+
+                        brands.forEach(brand -> {
+                            this.logger.log(Level.INFO, "Preparando: " + brand.getName());
+                            brand.setVehicleType(vehicleType);
+                        });
+
+                        return Mono.just(this.brandRepository.saveAll(brands));
                     } catch (JsonProcessingException e) {
                         this.logger.log(Level.SEVERE, "Erro ao converter JSON: " + e.getMessage());
+                        return Mono.error(e);
                     }
-                });
-
+                })
+                .subscribe(
+                        success -> this.logger.log(Level.INFO, "Todas as marcas de " + vehicleType.getDescription() + " foram salvas no banco de dados"),
+                        error -> this.logger.log(Level.SEVERE, "Erro ao salvar marcas de " + vehicleType.getDescription() + ": " + error.getMessage())
+                );
     }
 
-    private void populateMotorcycleBrands() {
-
+    private void populateModels() {
+        if (this.modelRepository.count() == 0) {
+            this.populateModelsByType("cars", VehicleType.CAR);
+            this.populateModelsByType("motorcycles", VehicleType.MOTORCYCLE);
+            this.populateModelsByType("trucks", VehicleType.TRUCK);
+        }
     }
 
-    private void populateTruckBrands() {
+    private void populateModelsByType(String typeStr, VehicleType vehicleType) {
+        List<Brand> brands = this.brandRepository.findAllByVehicleType(vehicleType);
 
+        brands.forEach(brand -> {
+            this.webClient.get()
+                    .uri("/" + typeStr + "/brands/" + brand.getCode() + "/models")
+                    .header("X-Subscription-Token", this.subscriptionToken)
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .flatMap(result -> {
+                        try {
+                            List<Model> models = this.objectMapper.readValue(result, new TypeReference<>() {});
+
+                            models.forEach(model -> {
+                                this.logger.log(Level.INFO, "Preparando: " + model.getName() + " , marca " + brand.getName());
+                                model.setBrand(brand);
+                            });
+
+                            return Mono.just(this.modelRepository.saveAll(models));
+                        } catch (JsonProcessingException e) {
+                            this.logger.log(Level.SEVERE, "Erro ao converter JSON: " + e.getMessage());
+                            return Mono.error(e);
+                        }
+                    })
+                    .subscribe(
+                            success -> this.logger.log(Level.INFO, "Todas os modelos de " + vehicleType.getDescription() + " da marca " + brand.getName() + " foram salvos no banco de dados"),
+                            error -> {
+                                this.logger.log(Level.SEVERE, "Erro ao salvar modelos de " + vehicleType.getDescription() + ": " + error.getMessage());
+                                throw new RuntimeException(error);
+                            }
+                    );
+        });
     }
 }
