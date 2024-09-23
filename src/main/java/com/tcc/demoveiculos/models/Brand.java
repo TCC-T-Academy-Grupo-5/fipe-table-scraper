4fb9d10224cb95ecc1228bcca6d00577b71d08d1
Paulo Henrique <ph.1928@hotmail.com>
2024-09-22 22:25:25 -0300

    fix: toString method in brand and model

--- src/main/java/com/tcc/demoveiculos/models/Brand.java
+++ src/main/java/com/tcc/demoveiculos/models/Brand.java
@@ -1,14 +1,18 @@
 package com.tcc.demoveiculos.models;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Data
+@Getter
+@Setter
 @Table(name = "brand")
 public class Brand {
 
@@ -25,5 +29,16 @@ public class Brand {
     private VehicleType vehicleType;
 
     @OneToMany(mappedBy = "brand", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonManagedReference
     private List<Model> models = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "Brand{" +
+                "vehicleType=" + vehicleType +
+                ", urlPathName='" + urlPathName + '\'' +
+                ", name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/tcc/demoveiculos/models/Model.java
+++ src/main/java/com/tcc/demoveiculos/models/Model.java
@@ -1,9 +1,13 @@
 package com.tcc.demoveiculos.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -19,7 +23,21 @@ public class Model {
 
     private String urlPathName;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "brand_id")
+    @JsonIgnore
     private Brand brand;
+
+    @OneToMany(mappedBy = "model", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Year> years = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "Model{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", urlPathName='" + urlPathName + '\'' +
+                ", brand=" + brand +
+                '}';
+    }
 }
