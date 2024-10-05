28b3e620e0e21e30181d3970b077d7680b00f7a8
kingabreel <gabrielyury441@gmail.com>
2024-10-04 21:36:21 -0300

    feat: prices added to vehicle

--- src/main/java/com/tcc/demoveiculos/models/Vehicle.java
+++ src/main/java/com/tcc/demoveiculos/models/Vehicle.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
+import java.util.List;
+
 @Entity
 @Data
 @Table(name = "vehicle")
@@ -15,6 +17,9 @@ public class Vehicle {
 
     @NotBlank
     private String fipeCode;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "vehicle")
+    private List<FipePrice> fipePrices;
 
     @OneToOne
     @JoinColumn(name = "year_id")
